bc313f8cc01afa7cfd92206501704e6471d30b2d
HsuTimo <72869492+HsuTimo@users.noreply.github.com>
2021-06-07 11:24:52 +0200

    Add project files.

---
+++ TimothyHsu_PartyMVC/Controllers/VisitorsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimothyHsu_PartyMVC.Models;
+
+namespace TimothyHsu_PartyMVC.Controllers
+{
+    public class VisitorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["ListLength"] = 0;
+            var list = new List<Visitor>();
+            TempData["List"] = JsonConvert.SerializeObject(list);
+            return View("Input");
+        }
+        [HttpGet]
+        public IActionResult Input()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Input(Visitor visitor)
+        {
+            var list = JsonConvert.DeserializeObject<List<Visitor>>((string)TempData.Peek("List"));
+            list.Add(visitor);
+            TempData["ListLength"] = list.Count;
+            TempData["List"] = JsonConvert.SerializeObject(list);
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            var list = JsonConvert.DeserializeObject<List<Visitor>>((string)TempData.Peek("List"));
+            TempData["TotalGuests"] = list.Count;
+            TempData["TotalFamily"] = list.Where(x => x.IsFamily).ToList().Count;
+            int youngestAge = list[0].Age;
+            foreach (var item in list)
+            {
+                if (item.Age<youngestAge)
+                {
+                    youngestAge = item.Age;
+                }
+            }
+            int oldestAge = list[0].Age;
+            foreach (var item in list)
+            {
+                if (item.Age > oldestAge)
+                {
+                    oldestAge = item.Age;
+                }
+            }
+            TempData["YoungestAge"] = youngestAge;
+            TempData["OldestAge"] = oldestAge;
+            return View();
+        }
+    }
+}
---
+++ TimothyHsu_PartyMVC/Models/Visitor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimothyHsu_PartyMVC.Models
+{
+    public class Visitor
+    {
+        [Display(Name = "First name")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname")]
+        [Required(ErrorMessage = "Please enter your surname")]
+        public string SecondName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Tel { get; set; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        [Range(18,60,ErrorMessage = "Please enter a value between 18 and 60")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter your postal code")]
+        [Range(1000, 1500, ErrorMessage = "Please enter a value between 1000 and 1500")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter your city")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please select an option")]
+        public bool IsFamily { get; set; }
+
+    }
+}
